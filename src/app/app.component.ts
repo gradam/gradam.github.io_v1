218611d63d92dfe4ffee2ddaa224850d185df362
Jakub Semik <kuba.semik@gmail.com>
2017-04-15 19:56:06 +0200

    work in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+///<reference path="../../node_modules/@types/jquery/index.d.ts"/>
+import { Component, OnInit } from '@angular/core';
+declare var $: JQueryStatic;
+declare var particlesJS: any;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title = 'JS - Full-stack developer / Data scientist';
+
+  ngOnInit(): void {
+    console.log('Init');
+    // Initialize particlesJS
+    particlesJS.load('particles-js', 'assets/js/particlesjs-config.json', function() {
+      console.log('callback - particles.js config loaded');
+    });
+  }
 }
