4681b6e85900bf6b4f83b949427cc8b67261da73
Jakub Semik <kuba.semik@gmail.com>
2017-04-20 15:05:09 +0200

    CV page done

--- src/app/CV/CV.component.ts
+++ src/app/CV/CV.component.ts
@@ -1,10 +1,16 @@
 import { Component } from "@angular/core";
+import { rubberAnimation } from "../rubber-animation";
 
 
 @Component({
   selector: 'CV',
   templateUrl: './CV.component.html',
+  styleUrls: ['./CV.component.css']
 })
 export class CVComponent {
 
+  mouseIn(event): void {
+    let target = $(event.target);
+    rubberAnimation(target);
+  }
 }
