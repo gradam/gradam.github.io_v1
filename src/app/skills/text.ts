aaa0e06136dcd3880a52b58e14f5a58615840251
Jakub Semik <kuba.semik@gmail.com>
2017-04-26 20:13:04 +0200

    Updated texts

--- src/app/skills/text.ts
+++ src/app/skills/text.ts
@@ -1,7 +1,6 @@
 export const ABOUT: string[] = [
-  `I have vast range of technical skills ranging from Angular and HTML in front-end to Django and Python in back-end. 
+  `I have a vast range of technical skills ranging from front-end technologies such as Angular and HTML through Django and Python for back-end development.
   My latest projects are based on Django, Angular and Amazon Web Services`,
-  `However, working as a developer is not just about the code. It's even more about good communication and collaboration with the rest of the team,
-  I am a huge fan of tools like slack or trello and I encourage every team to use those.
-   `
+  `However, working as a developer is not just about the code. It's even more about effective communication and collaboration with the rest of the team,
+  I am a huge fan of tools like Slack or Trello and I encourage every team to use them.`
 ];
