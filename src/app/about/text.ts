ae0c45a1781b36a21793c70c77e9f8c3b533a191
Jakub Semik <kuba.semik@gmail.com>
2017-04-26 22:17:17 +0200

    Fixed typo

--- src/app/about/text.ts
+++ src/app/about/text.ts
@@ -1,5 +1,5 @@
 export const ABOUT: string[] = [
-  'Hey, I\'m a 19 years old programmer from Poland passionate about code, startups, technology and science.',
+  'Hey, I\'m a 19 year old programmer from Poland passionate about code, startups, technology and science.',
   'For the last 4 years I\'ve been fascinated with cutting-edge technologies and I\'ve been specializing in programming with Python, ' +
   'lately I\'ve been focused mostly on a back-end development with Django web framework.',
   'In addition I like to immerse myself in a front-end development (HTML/CSS/JS, Angular) or data analysis and visualisation in Python.'
