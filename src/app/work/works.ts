ff9756c0b9d549bcb0f4cc5b79e81ba4851ce654
Jakub Semik <kuba.semik@gmail.com>
2017-05-21 11:04:46 +0200

    Changed veeu link cuz the old one is not working anymore

--- src/app/work/works.ts
+++ src/app/work/works.ts
@@ -3,6 +3,6 @@ import { PORTFOLIO_PAGE_TEXT } from "./texts/portfolio-page";
 
 
 export const WORKS = [
-  {name: 'VeeU', image: '../../assets/img/work/veeu/logo.png', link: 'http://veeu.co/', text: VEEU_TEXT, id: 1},
+  {name: 'VeeU', image: '../../assets/img/work/veeu/logo.png', link: 'http://www.veeu.co/', text: VEEU_TEXT, id: 1},
   {name: 'Portfolio page', image: '../../assets/img/work/portfolio-page/logo.png', link: 'https://jakubsemik.com', text: PORTFOLIO_PAGE_TEXT, id: 2},
 ];
